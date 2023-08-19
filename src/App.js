4cdb8129213e59d439e8c1909564b0eea7e2ebac
Nijanshu <46223306+Nijanshu@users.noreply.github.com>
2023-08-20 02:18:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+// App.js
 import './App.css';
+import React, { Component } from 'react';
+import News from './Components/news';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+         {/* <News/> */}
+          <Routes>
+          
+            <Route path="/sports" element={<News key="sports" country="in" category="sports" />} />
+            <Route path="/entertainment" element={<News key="entertainment" country="in" category="entertainment" />} />
+            <Route path="/science" element={<News key="science" country="in" category="science" />} />
+            <Route path="/technology" element={<News key="technology" country="in" category="technology" />} />
+            <Route path='/' element={<News key="general" country="in" category="general" />} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
